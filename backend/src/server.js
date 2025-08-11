20122c5f15f4a079ecc9503c8b4b216d952a16eb
Siddhi Agrawal <siagrawa@progress.com>
2025-08-11 15:43:44 +0530

    up and running

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,24 @@ import dotenv from 'dotenv';
 import { initDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionRoute from './routes/transactionRoute.js';
+import job from './config/cron.js';   
 dotenv.config();
 const app = express()
+if(process.env.NODE_ENV === 'production') {
+  job.start();
+  console.log("Cron job started");
+}
 // Middleware
 app.use(rateLimiter);
 app.use(express.json());
 
+const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use('/api/transactions', transactionRoute);
-const PORT = process.env.PORT || 5001;
 initDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- mobile_wallet/constants/api.js
+++ mobile_wallet/constants/api.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://192.168.0.196:5001/api";
+// export const API_URL = "https://wallet-api-j74x.onrender.com/api";
+export const API_URL = "http://192.168.0.196:5001/api"; // Use your machine's local IP for device/emulator access
